4baac82fd7463e29e7ba9c39d4851d75d6d881ba
Zachary Siefkes <zacharysiefkes@gmail.com>
2021-03-26 16:14:35 +1300

    Create image pipeline

--- index.ts
+++ index.ts
@@ -2,8 +2,15 @@ import * as pulumi from "@pulumi/pulumi";
 import * as aws from "@pulumi/aws";
 import * as awsx from "@pulumi/awsx";
 
-// Create an AWS resource (S3 Bucket)
-const bucket = new aws.s3.Bucket("my-bucket");
+const testImagePipeline = new aws.imagebuilder.ImagePipeline(
+    "testPulumiImagePipeline",
+    {
+        imageRecipeArn: "arn:aws:imagebuilder:us-east-1:271428618339:image-recipe/first-recipe/0.1.0",
+        infrastructureConfigurationArn: "arn:aws:imagebuilder:us-east-1:271428618339:infrastructure-configuration/first-infra-config",
+        schedule: {
+            scheduleExpression: "cron(0 0 ? * 7 *)"
+        }
+    }
+);
 
-// Export the name of the bucket
-export const bucketName = bucket.id;
+export const imagePipelineName = testImagePipeline.id;
