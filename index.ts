aa9b42cf09ff0c2f8269a1097cc0e80c03558cca
Zachary Siefkes <zacharysiefkes@gmail.com>
2021-03-29 16:30:07 +1300

    Create image recipe using build component and use for image pipeline

--- index.ts
+++ index.ts
@@ -17,8 +17,6 @@ const bucketObject = new aws.s3.BucketObject(
 
 // Export bucket uri
 export const buildBucketUri = pulumi.interpolate`s3://${yamlBuildBucket.id}/${buildBucketName}`;
-// the Pulumi bucket object doesn't have an s3 uri attribute. the format is above. desired result: "s3://build-config-029b0ff/testBuildConfig"
-// export const buildBucketUri = pulumi.interpolate`s3://${yamlBuildBucket.id}/${yamlBuildBucket.bucket}`; // this one should work but .bucket is returning the same is .id :S shouldn't. should return the name I set.
 
 // Create build component
 const testBuildComponent = new aws.imagebuilder.Component(
@@ -26,19 +24,28 @@ const testBuildComponent = new aws.imagebuilder.Component(
     {
         platform: "Windows",
         version: "0.1.0",
-        // for build steps, can either specify exactly one out of data (YAML format) or uri to the YAML file (S3 component)
+        // can specify exactly one out of data and uri attributes. data should be in YAML format; uri should be an S3 uri to a component containing YAML spec)
         uri: buildBucketUri
     }
 );
 
-// create an image recipe using this build component
-// TODO
+// Create an image recipe using above build component
+const testImageRecipe = new aws.imagebuilder.ImageRecipe(
+    "testImageRecipe",
+    {
+        components: [{
+            componentArn: testBuildComponent.arn
+        }],
+        parentImage: "arn:aws:imagebuilder:us-east-1:aws:image/windows-server-2019-english-full-base-x86/x.x.x",
+        version: "0.1.0"
+    }
+)
 
-// Create image pipeline using existing image recipe and infrastructure config
+// Create image pipeline using above image recipe and separately created infrastructure config
 const testImagePipeline = new aws.imagebuilder.ImagePipeline(
     "testPulumiImagePipeline",
     {
-        imageRecipeArn: "arn:aws:imagebuilder:us-east-1:271428618339:image-recipe/first-recipe/0.1.0",
+        imageRecipeArn: testImageRecipe.arn,
         infrastructureConfigurationArn: "arn:aws:imagebuilder:us-east-1:271428618339:infrastructure-configuration/first-infra-config",
         schedule: {
             scheduleExpression: "cron(0 0 ? * 7 *)"
@@ -46,7 +53,5 @@ const testImagePipeline = new aws.imagebuilder.ImagePipeline(
     }
 );
 
-
-
-// Export image pipeline name
-export const imagePipelineName = testImagePipeline.id;
+// Export image pipeline arn
+export const imageRecipeArn = testImagePipeline.arn;
